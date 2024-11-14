f68d5248647ec0a06812de0d317c2a67c72807f2
MoayadAlismail <moayadalismail@gmail.com>
2024-11-14 08:52:58 -0500

    .

--- exercise6/components/LoginForm/index.js
+++ exercise6/components/LoginForm/index.js
@@ -1,15 +1,33 @@
+import styles from '../styles/LoginForm.module.css';
+
 export default function CreateUserForm() {
-    return(
-        <div>
-        <form>
-            <label htmlFor="email">Email:</label>
-            <input type="email" name="email" required />
-            
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" required />
-            
-            <button type="submit">Create User</button>
-        </form>
+    return (
+        <div className={styles.formContainer}>
+            <form className={styles.form}>
+                <div className={styles.formGroup}>
+                    <label className={styles.label} htmlFor="email">Email:</label>
+                    <input 
+                        className={styles.input}
+                        type="email" 
+                        name="email" 
+                        required 
+                    />
+                </div>
+                
+                <div className={styles.formGroup}>
+                    <label className={styles.label} htmlFor="password">Password:</label>
+                    <input 
+                        className={styles.input}
+                        type="password" 
+                        name="password" 
+                        required 
+                    />
+                </div>
+                
+                <button className={styles.button} type="submit">
+                    Create User
+                </button>
+            </form>
         </div>
     );
 }
